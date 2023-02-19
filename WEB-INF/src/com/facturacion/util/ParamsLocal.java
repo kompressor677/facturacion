8d180f38217986cc8f33d9ec92c33b8c4ed9486d
Administrador <Administrador@quetzalcoatl.petropar.gov.py>
2023-02-19 01:34:28 -0300

    Se crearon los modelos de:

--- WEB-INF/src/com/facturacion/util/ParamsLocal.java
+++ WEB-INF/src/com/facturacion/util/ParamsLocal.java
@@ -20,5 +20,26 @@ public class ParamsLocal extends Params{
 	public static final String OP_CREAR_PRUEBA = "CrearPrueba";
 	public static final String OP_EDITAR_PRUEBA = "EditarPrueba";
 	public static final String OP_BORRAR_PRUEBA = "BorrarPrueba";
+	
+	public static final String OP_CREAR_CLIENTE = "CrearCliente";
+	public static final String OP_EDITAR_CLIENTE = "EditarCliente";
+	public static final String OP_BORRAR_CLIENTE = "BorrarCliente";
+	
+	public static final String OP_CREAR_PROVEEDOR = "CrearProveedor";
+	public static final String OP_EDITAR_PROVEEDOR = "EditarProveedor";
+	public static final String OP_BORRAR_PROVEEDOR = "BorrarProveedor";
+	
+	public static final String OP_CREAR_TIMBRADO = "CrearTimbrado";
+	public static final String OP_EDITAR_TIMBRADO = "EditarTimbrado";
+	public static final String OP_BORRAR_TIMBRADO = "BorrarTimbrado";
+	
+	public static final String OP_CREAR_MARCA = "CrearMarca";
+	public static final String OP_EDITAR_MARCA = "EditarMarca";
+	public static final String OP_BORRAR_MARCA = "BorrarMarca";
+	
+	public static final String OP_CREAR_PRODUCTO = "CrearProducto";
+	public static final String OP_EDITAR_PRODUCTO = "EditarProducto";
+	public static final String OP_BORRAR_PRODUCTO = "BorrarProducto";
+
 
 }
